f2f89cab4a698ec01cac64dd6135f121339778ad
William Mamede <williamsm@inatel.br>
2019-11-26 15:45:44 -0200

    Using property binding

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -3,14 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   // selector: '[app-servers]',
   selector: '.app-servers',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>`,
+  templateUrl: './servers.component.html',
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServe = false;
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServe = true;
+    }, 2000);
+  }
 
   ngOnInit() {
   }
