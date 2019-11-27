d9d0145dc16fced5d774742d15fc6abbedc9db95
William Mamede <williamsm@inatel.br>
2019-11-27 14:35:45 -0200

    Added ngStyle and ngClass directive

--- my-first-app/src/app/server/serve.component.ts
+++ my-first-app/src/app/server/serve.component.ts
@@ -2,13 +2,26 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-server',
-    templateUrl: './serve.component.html'
+    templateUrl: './serve.component.html',
+    styles: [`
+        .online {
+            color: white
+        }
+    `]
 })
 export class ServerComponent {
     serverId: number = 10;
-    serverStatus: string = 'offline'
+    serverStatus: string = 'offline';
+
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    }
 
     getServerStatus() {
         return this.serverStatus;
+    }
+
+    getColor() {
+        return this.serverStatus === 'online' ? 'green'  : 'red';
     }
 }
