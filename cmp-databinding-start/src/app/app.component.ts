8b151cbb5a29f5f6a23930c1609e50ea10f12d3d
William Mamede <williamsm@inatel.br>
2019-12-09 14:46:09 -0200

    Create output event
    Created a output decorator in a property that will
    represents an event

--- cmp-databinding-start/src/app/app.component.ts
+++ cmp-databinding-start/src/app/app.component.ts
@@ -8,4 +8,19 @@ import { Component } from '@angular/core';
 export class AppComponent {
   serverElements = [{type: 'server', name: 'testServer', content: 'just a test!'}];
 
+  onServerAdded(serverDate: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverDate.serverName,
+      content: serverDate.serverContent
+    });
+  }
+
+  onBlueprintAdded(serverDate: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: serverDate.serverName,
+      content: serverDate.serverContent
+    });
+  }
 }
